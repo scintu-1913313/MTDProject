1bc1806f540fbca261c210a1cff57dba7f1c1cfc
scintu.1913313@studenti.uniroma1.it <scintu.1913313@studenti.uniroma1.it>
2025-10-20 22:50:51 +0200

    generati javadoc

--- MTDProject/doc/Javadoc/package-search-index.js
+++ MTDProject/doc/Javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"carte"},{"l":"controller"},{"l":"JTressette"},{"l":"model"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"carte"},{"l":"controller"},{"l":"JTressette"},{"l":"model"},{"l":"view"}];updateSearchResults();
